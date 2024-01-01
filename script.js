44105dd5126bc56d19579553a59c935dbdb58995
rohan7745 <138517784+rohan7745@users.noreply.github.com>
2024-01-01 19:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create student object
+const student = {
+  name: 'John Doe'
+};
+
+// Adding getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Example usage:
+console.log(student.getKeys()); // Output: ['name']
